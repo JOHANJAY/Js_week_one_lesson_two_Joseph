a581ae45b57ef88040ec7c8997cf7fa37fc0c15b
John Joseph <joseph.john33@yahoo.com>
2024-09-08 18:24:44 +0100

    submiting weekone lessontwo

---
+++ index.js
@@ -0,0 +1,91 @@
+//Task One
+/* let num = 1
+
+if (num > 10) {
+    console.log('You pass');
+} */
+
+//Task Two
+/* let num = 10;
+if (num % 2 == 0) {
+  console.log("your num is even");
+} else {
+  console.log("your num is odd");
+} */
+
+//Task Three
+/* let num = 1
+
+if (num < 0) {
+    console.log('you have a negative num');
+    
+} else if (num > 0) {
+    console.log('you have a positive num');
+    
+} else {
+    console.log('you have just 0');
+    
+} */
+
+//Task Four
+/* let num1 = 25;
+let num2 = 12;
+
+if (num1 > 0) {
+  console.log("The number is positve");
+
+  if (num2 > 0) {
+    console.log("The numbers are positive");
+  } else {
+    console.log("The number is negative");
+  }
+} else {
+  console.log("The number is negative");
+}
+ */
+
+//Task Five
+/* let num = 10
+
+num > 10 ? console.log('Your number is greater than 10') : num < 10 ? console.log('Your number is less than 10'): console.log('You have a 10');
+; */
+
+//Task Six
+/* let num = 6
+
+if (num >= 5 && num <= 10) {
+    console.log('you are within the needed range');
+    
+} else {
+    console.log("you are outside the needed range");
+} */
+
+//Task Seven
+/* let day = 2
+
+switch (day) {
+    case 1:
+        console.log('Today is Monday')
+        break;
+    case 2:
+        console.log('Today is Tueseday')
+        break;
+    case 3:
+        console.log('Today is Wednesday')
+        break;
+    case 4:
+        console.log('Today is Thursday')
+        break;
+    case 5:
+        console.log('Today is Friday')
+        break;
+    case 6:
+        console.log('Today is Saturday')
+        break;
+    case 7:
+        console.log('Today is Sunday')
+        break;
+
+    default: console.log('There are only seven days in a week and that is not one of them')
+        break;
+} */
